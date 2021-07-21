ed2f15cc966b1c837f4ecbea648aa2d76303174a
karuu-01 <brian.muchori@student.moringaschool.com>
2021-07-22 01:23:10 +0300

    add all changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,11 +11,13 @@ import { SearchDetailComponent } from './search-detail/search-detail.component';
   declarations: [
     AppComponent,
     GiphyTitleComponent,
-    SearchDetailComponent
+    SearchDetailComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  GiphyApiKey: "dG1BxqPXJwicd5aI77gkbsAiRnLrhqBK"
 };
 
 /*
