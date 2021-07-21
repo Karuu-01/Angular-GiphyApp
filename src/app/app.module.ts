6363ef380c79db5faed8ab9cc59331134bf54591
karuu-01 <brian.muchori@student.moringaschool.com>
2021-07-22 00:33:29 +0300

    add changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GiphyTitleComponent } from './giphy-title/giphy-title.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GiphyTitleComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/giphy-title/giphy-title.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-giphy-title',
+  templateUrl: './giphy-title.component.html',
+  styleUrls: ['./giphy-title.component.css']
+})
+export class GiphyTitleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
